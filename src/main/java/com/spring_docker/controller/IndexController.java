cde201ef0f86ee9ef60fe0d8501d3ef38cac2719
Tan <18473015994@163.com>
2018-10-09 16:10:47 +0800

    哈哈

--- src/main/java/com/spring_docker/controller/IndexController.java
+++ src/main/java/com/spring_docker/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.spring_docker.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by leung on 2018/9/29.
  */
 @RestController
+@Slf4j
 public class IndexController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping(value = {"/","/index"})
     public String index(){
-        return "hello1112";
+        log.info("hello----------------------");
+        //stringRedisTemplate.opsForValue().set("hello","体育个偶尔温柔群殴批娃儿");
+        return stringRedisTemplate.opsForValue().get("hello");
     }
 }
