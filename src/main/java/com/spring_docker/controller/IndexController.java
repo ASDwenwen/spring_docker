aadb90a5842aaca56e02b4bd269daa152fff143b
Tan <18473015994@163.com>
2018-10-10 11:09:49 +0800

    哈哈

--- src/main/java/com/spring_docker/controller/IndexController.java
+++ src/main/java/com/spring_docker/controller/IndexController.java
@@ -16,6 +16,6 @@ public class IndexController {
     @GetMapping(value = {"/","/index"})
     public String index(){
         log.info("hello----------------------");
-        return "hello world哈哈哈";
+        return "hello world1122哈哈哈";
     }
 }
